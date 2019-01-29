d8834df81a95db7ff85406eca65ab39b631127f9
alcr33k <alex.alle@hotmail.com>
2019-01-29 10:44:46 +0100

    Changed file names

---
+++ settings.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="results.php">
+        <label for="apor">Apor</label>
+        <input type="number" name="apor"> 
+        <label for="giraffer">Giraffer</label>
+        <input type="number" name="giraffer"> 
+        <label for="tigrar">Tigrar</label>
+        <input type="number" name="tigrar"> 
+        <label for="kokosnötter">Kokosnötter</label>
+        <input type="number" name="kokosnötter" placeholder="Antal kokosnötter"> 
+        <input type="submit" value="Skapa naturreservat">
+    </form>
+</body>
+</html>
