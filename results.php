7a7c8036de3198a06c648baeff1bb73de7f92920
alcr33k <alex.alle@hotmail.com>
2019-01-29 10:50:42 +0100

    Can't see results unless settingsform sent

--- results.php
+++ results.php
@@ -1,57 +1,62 @@
 <?php
-    abstract class Animal {
-        abstract protected function makeSound();
-    }
-    class Apa extends Animal {
-        private $sound = 'Makak';
-        private $name = 'Apa';
-        function makeSound() {
-            return 'En ' .$this->name . ' säger ' . $this->sound;
-        }   
-    }
-    class Giraff extends Animal {
-        private $sound = 'SchloSchloSchlo';                                   
-        private $name = 'Giraff';
-        function makeSound() {
-            return 'En ' .$this->name . ' säger ' . $this->sound;
-        }                                 
-    }
-    class Kokosnöt extends Animal {
-        private $sound ='KLA KLO';
-        private $name ='Kokosnöt';       
-        function makeSound() {
-            return 'En ' .$this->name . ' säger ' . $this->sound;
-        }                               
-    }
-    class Tiger extends Animal {
-        private $sound = 'Rawr';
-        private $name = 'Tiger';
-        function makeSound() {
-            return 'En ' .$this->name . ' säger ' . $this->sound;
-        }   
-    }
+    if (isset($_SESSION['formSent'])) {
+        abstract class Animal {
+            abstract protected function makeSound();
+        }
+        class Apa extends Animal {
+            private $sound = 'Makak';
+            private $name = 'Apa';
+            function makeSound() {
+                return 'En ' .$this->name . ' säger ' . $this->sound;
+            }   
+        }
+        class Giraff extends Animal {
+            private $sound = 'SchloSchloSchlo';                                   
+            private $name = 'Giraff';
+            function makeSound() {
+                return 'En ' .$this->name . ' säger ' . $this->sound;
+            }                                 
+        }
+        class Kokosnöt extends Animal {
+            private $sound ='KLA KLO';
+            private $name ='Kokosnöt';       
+            function makeSound() {
+                return 'En ' .$this->name . ' säger ' . $this->sound;
+            }                               
+        }
+        class Tiger extends Animal {
+            private $sound = 'Rawr';
+            private $name = 'Tiger';
+            function makeSound() {
+                return 'En ' .$this->name . ' säger ' . $this->sound;
+            }   
+        }
 
-    $numApor = $_POST["apor"];
-    $numGiraffer = $_POST["giraffer"];
-    $numTigrar = $_POST["tigrar"];
-    $numKokosnötter = $_POST["kokosnötter"];
-    $giraff = new Giraff();
-    $apa = new Apa();
-    $tiger = new Tiger();
-    $kokosNöt = new Kokosnöt();
-    
-    $toAdd = "";
-    for($i=0; $i < $numApor; $i++) {
-        $toAdd .= '<img src="src/apa.jpg" alt="apa" class="apa" onClick="alert(\''. $apa->makeSound() .'\');">';
-    }
-    for($i=0; $i < $numGiraffer; $i++) {
-        $toAdd .= '<img src="src/giraff.jpg" alt="giraff" class="giraff" onClick="alert(\''. $giraff->makeSound() .'\');">';
-    }
-    for($i=0; $i < $numTigrar; $i++) {
-        $toAdd .= '<img src="src/tiger.jpg" alt="tiger" class="tiger" onClick="alert(\''. $tiger->makeSound() .'\');">';
-    }
-    for($i=0; $i < $numKokosnötter; $i++) {
-        $toAdd .= '<img src="src/kokosnöt.jpg" alt="kokosnöt" class="kokosnöt" onClick="alert(\''. $kokosNöt->makeSound() .'\');">';
+        $numApor = $_POST["apor"];
+        $numGiraffer = $_POST["giraffer"];
+        $numTigrar = $_POST["tigrar"];
+        $numKokosnötter = $_POST["kokosnötter"];
+        $giraff = new Giraff();
+        $apa = new Apa();
+        $tiger = new Tiger();
+        $kokosNöt = new Kokosnöt();
+        
+        $toAdd = "";
+        for($i=0; $i < $numApor; $i++) {
+            $toAdd .= '<img src="src/apa.jpg" alt="apa" class="apa" onClick="alert(\''. $apa->makeSound() .'\');">';
+        }
+        for($i=0; $i < $numGiraffer; $i++) {
+            $toAdd .= '<img src="src/giraff.jpg" alt="giraff" class="giraff" onClick="alert(\''. $giraff->makeSound() .'\');">';
+        }
+        for($i=0; $i < $numTigrar; $i++) {
+            $toAdd .= '<img src="src/tiger.jpg" alt="tiger" class="tiger" onClick="alert(\''. $tiger->makeSound() .'\');">';
+        }
+        for($i=0; $i < $numKokosnötter; $i++) {
+            $toAdd .= '<img src="src/kokosnöt.jpg" alt="kokosnöt" class="kokosnöt" onClick="alert(\''. $kokosNöt->makeSound() .'\');">';
+        }
+    } else {
+        header('Location: index.php');
+        exit();
     }
 ?>
 <!DOCTYPE html>
